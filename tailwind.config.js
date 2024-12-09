b903d7aac1b3a40e718c89a9f845b578c5dc711c
Ngọc Trần <ngoctran250426@gmail.com>
2024-12-09 15:05:12 +0700

    mupdate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+import scrollbar from "tailwind-scrollbar";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {},
-  plugins: [
-    // .prettierrc
-    {
-      tailwindConfig: "./styles/tailwind.config.js",
-    },
-    require("tailwind-scrollbar")({ nocompatible: true }),
-  ],
+  plugins: [scrollbar({ nocompatible: true })],
 };
